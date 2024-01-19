a6cbd8ae6b1ef4f0067f9cb6e888849c90d5e506
WilliamTavers <154641617+WilliamTavers@users.noreply.github.com>
2024-01-19 14:05:36 +1030

    Update index.ts

--- clone/video-processing-service/src/index.ts
+++ clone/video-processing-service/src/index.ts
@@ -18,3 +18,43 @@ export const createUser = functions.auth.user().onCreate((user) => {
   logger.info(`User Created: ${JSON.stringify(userInfo)}`);
   return;
 });
+
+import { isVideoNew, setVideo } from "./firestore";
+
+app.post("/process-video", async (req, res) => {
+  ...
+
+  const inputFileName = data.name; // In format of <UID>-<DATE>.<EXTENSION>
+  const outputFileName = `processed-${inputFileName}`;
+  const videoId = inputFileName.split('.')[0];
+
+  if (!isVideoNew(videoId)) {
+    return res.status(400).send('Bad Request: video already processing or processed.');
+  } else {
+    await setVideo(videoId, {
+      id: videoId,
+      uid: videoId.split('-')[0],
+      status: 'processing'
+    });
+  }
+
+  // Download the raw video from Cloud Storage
+  await downloadRawVideo(inputFileName);
+
+  ...
+
+  // Upload the processed video to Cloud Storage
+  await uploadProcessedVideo(outputFileName);
+
+  await setVideo(videoId, {
+    status: 'processed',
+    filename: outputFileName
+  });
+
+  await Promise.all([
+    deleteRawVideo(inputFileName),
+    deleteProcessedVideo(outputFileName)
+  ]);
+
+  return res.status(200).send('Processing finished successfully');
+});
