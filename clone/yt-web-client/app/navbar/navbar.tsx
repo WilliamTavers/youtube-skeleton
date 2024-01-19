74478e7572996a604b5f6b0069e90c7f43e4cb21
WilliamTavers <154641617+WilliamTavers@users.noreply.github.com>
2024-01-19 14:02:38 +1030

    Update navbar.tsx

--- clone/yt-web-client/app/navbar/navbar.tsx
+++ clone/yt-web-client/app/navbar/navbar.tsx
@@ -25,15 +25,17 @@ function NavBar() {
 
 
   return (
-    <nav className={styles.nav}>
-      <Link href="/">
-        <span className={styles.logoContainer}>
-          <img className={styles.logo} src="/youtube-logo.svg" alt="YouTube Logo" />
-        </span>
-      </Link>
-      <SignIn user={user} />
-    </nav>
-  );
-}
+  <nav className={styles.nav}>
+    <Link href="/">
+      <Image width={90} height={20}
+        src="/youtube-logo.svg" alt="YouTube Logo"/>
+    </Link>
+    { 
+      user && <Upload />
+    }
+    <SignIn user={user} />
+  </nav>
+);
+
 
 export default NavBar;
