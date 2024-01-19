06c35e0522e752c8e64c1d8a7d2fa14bfc0544f4
WilliamTavers <154641617+WilliamTavers@users.noreply.github.com>
2024-01-19 14:06:15 +1030

    Update index.ts

--- clone/video-processing-service/src/index.ts
+++ clone/video-processing-service/src/index.ts
@@ -58,3 +58,21 @@ app.post("/process-video", async (req, res) => {
 
   return res.status(200).send('Processing finished successfully');
 });
+
+const videoCollectionId = "videos";
+
+export interface Video {
+  id?: string,
+  uid?: string,
+  filename?: string,
+  status?: "processing" | "processed",
+  title?: string,
+  description?: string
+}
+
+export const getVideos = onCall({maxInstances: 1}, async () => {
+  const querySnapshot =
+    await firestore.collection(videoCollectionId).limit(10).get();
+  return querySnapshot.docs.map((doc) => doc.data());
+});
+
